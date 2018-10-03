06858135a553cf75fc05e734dbb74306fa4eca21
DERRIFORD\martinan <amartin10@nhs.net>
2018-10-03 14:34:38 +0100

    03 10 18 No.24

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ const getJsLicense = require('./licenses/getJsLicense.js');
 
 prompt.start()
 
-prompt.get('licenseType', function (err, result) {
-    console.info(result);
+const licenseType = '';
+const briefDescription = '';
+const firstname = '';
+const lastname = '';
+
+prompt.get('Enter license type ...', function (err, result) {
+    licenseType = result;
+    console.info(licenseType);
+});
+
+prompt.get('Enter brief description of app ...', function (err, result) {
+    briefDescription = result;
+    console.info(briefDescription);
+});
+
+prompt.get('Enter first name ...', function (err, result) {
+    firstname = result;
+    console.info(firstname);
+});
+
+prompt.get('Enter last name ...', function (err, result) {
+    lastname = result;
+    console.info(lastname);
 });
 
 // const licenseType = alert('Enter license type ...');
@@ -15,9 +36,9 @@ prompt.get('licenseType', function (err, result) {
 // const firstname = alert('Enter first name ...');
 // const lastname = alert('Enter last name ...');
 
-// const licenseText = getJsLicense(licenseType, briefDescription, firstname, lastname);
+const licenseText = getJsLicense(licenseType, briefDescription, firstname, lastname);
 
-// console.info(licenseText);
+console.info(licenseText);
 
 
 
