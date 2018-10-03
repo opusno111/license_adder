92e197c9a6e6649f34f29d33f630d21d1118e690
DERRIFORD\martinan <amartin10@nhs.net>
2018-10-03 09:22:45 +0100

    03 10 18 No.1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const getLicense = require('./licenses/getLicense.js');
 
-const wibbleString = getLicense(someText);
+const conArgv = ['string'];
+
+
+const wibbleString = getLicense(conArgv[0]);
 
 console.log(wibbleString);
