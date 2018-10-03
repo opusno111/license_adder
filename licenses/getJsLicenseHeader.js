1ec48d3409d55b6feb3030a698010d92dfb695b1
DERRIFORD\martinan <amartin10@nhs.net>
2018-10-03 15:58:05 +0100

    03 10 18 No.40

--- licenses/getJsLicenseHeader.js
+++ licenses/getJsLicenseHeader.js
@@ -8,7 +8,7 @@ const agpl = (firstname, lastname, briefDescription) => {
         *
         *You should have received a copy of the GNU Affero General Public License
         *along with this program.  If not, see <http://www.gnu.org/licenses/>.
-        *  
+        */
     `
 }
 
