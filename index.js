c9955872c799574cd85dfe9afae9c007f1a00ede
DERRIFORD\martinan <amartin10@nhs.net>
2018-10-03 13:27:49 +0100

    03 10 18 No.19

--- index.js
+++ index.js
@@ -5,44 +5,14 @@ const program = require('commander');
 
 const getJsLicense = require('./licenses/getJsLicense.js');
 
-program
-    .version('2.15.0')
-    .description('Add license header to all file types in solution.');
+const licenseType = prompt('Enter license type ...');
+const briefDescription = prompt('Enter brief description of app ...');
+const firstname = prompt('Enter first name ...');
+const lastname = prompt('Enter last name ...');
 
-// Craft questions to users
-const questions = [
-    {
-        type: 'input',
-        name: 'licenseType',
-        message: 'Enter license type ...'
-    },
-    {
-        type: 'input',
-        name: 'briefDescription',
-        message: 'Enter brief description of app ...'
-    },
-    {
-        type: 'input',
-        name: 'firstname',
-        message: 'Enter firstname ...'
-    },
-    {
-        type: 'input',
-        name: 'surname',
-        message: 'Enter surname ...'
-    },
-]
+const licenseText = getJsLicense(licenseType, briefDescription, firstname, lastname);
 
-program
-    .command('getJsLicense')
-    .alias('a')
-    .description('Add License header to .js files')
-    .action(() => {
-        prompt(questions).then(answers =>
-        licenseJs = getJsLicense(answers),
-         console.info(licenseJs)
-        );
-    });
+console.prompt(licenseText);
 
 
 
