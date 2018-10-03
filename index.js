0b6e675841704e9f78712617ae18b87e03bd9f02
DERRIFORD\martinan <amartin10@nhs.net>
2018-10-03 13:29:39 +0100

    03 10 18 No.20

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const program = require('commander');
 
 const getJsLicense = require('./licenses/getJsLicense.js');
 
-const licenseType = prompt('Enter license type ...');
-const briefDescription = prompt('Enter brief description of app ...');
-const firstname = prompt('Enter first name ...');
-const lastname = prompt('Enter last name ...');
+const licenseType = alert('Enter license type ...');
+const briefDescription = alert('Enter brief description of app ...');
+const firstname = alert('Enter first name ...');
+const lastname = alert('Enter last name ...');
 
 const licenseText = getJsLicense(licenseType, briefDescription, firstname, lastname);
 
