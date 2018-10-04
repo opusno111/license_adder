936932539c34a2307249d70489c8b1a83a2b3596
DERRIFORD\martinan <amartin10@nhs.net>
2018-10-04 09:06:43 +0100

    04 10 18 No.2

--- utils/appendJsTsCsharpFiles.js
+++ utils/appendJsTsCsharpFiles.js
@@ -5,7 +5,7 @@ const prepend = require('prepend-file');
 const removeNodeModules = require('./removeNodeModules');
 
 const appendFiles = (licenseHeader) => {
-    find.file(/\.((ts)|(js)|(cs))$/i, process.cwd(), (files) => {
+    find.file(/\.((ts)|(js)|(cs)|(css)|(scss)|(sass))$/i, process.cwd(), (files) => {
         let JSfiles = removeNodeModules(files);
         JSfiles.forEach((fileName) => {
             prepend(fileName, licenseHeader, (err) => {
